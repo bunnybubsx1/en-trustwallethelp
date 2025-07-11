f37de0b508b1ff174c94c14ecb1172d9553abd1b
bunnybubsx1 <bunnybubsstore@gmail.com>
2025-07-11 15:56:03 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Canon Support'
+project = 'Trst Wallets'
 
 
 # The full version, including alpha/beta/rc tags
